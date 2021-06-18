07e949392bcbb0b605409ddc502bbed0f97513af
Victor <filyk1010@gmail.com>
2021-06-18 20:08:28 +0300

    Auth page UI almost finished

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import { NextPage } from "next"
 import Head from "next/head"
-import Link from "next/link"
+import Link from "next/Link"
 import Image from "next/image"
 import React from "react"
 import styled, { css } from "styled-components"
-import TwitterLogoSVG from "../components/svg/TwitterLogo"
 import AuthPageBackgroundPNG from "../public/images/auth-page-background.png"
 import Meta from "../components/Meta"
 
@@ -30,7 +29,7 @@ const AuthButton = styled.button<AuthButtonProps>`
   color: ${(props) => (props.btnType === "signup" ? "#fff" : "#1da1f2")};
   border: ${(props) =>
     props.btnType === "signup" ? "none" : "1px solid #1da1f2"};
-  margin-bottom: ${(props) => props.btnType === "signup" ? "20px" : "0"};
+  margin-bottom: ${(props) => (props.btnType === "signup" ? "20px" : "0")};
 `
 
 const MainWrapper = styled.main`
@@ -45,15 +44,11 @@ const MainBlock = styled.div`
   height: 95vh;
 `
 
-const NavbarBlock = styled.div`
-  margin-top: auto;
-`
-
 const AuthBlock = styled.div`
   order: 2;
   height: 100%;
   width: 45vw;
-  padding: 180px 0 0 40px;
+  padding: 215px 0 0 50px;
 `
 
 const StyledSmallTwitterLogo = styled(Image)`
@@ -62,16 +57,16 @@ const StyledSmallTwitterLogo = styled(Image)`
 `
 
 const BigHeader = styled.h2`
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica,
-    Arial, sans-serif;
+  font-family: -apple-system, BNavbarLinkMacSystemFont, "Segoe UI", Roboto,
+    Helvetica, Arial, sans-serif;
   font-weight: 700;
   font-size: 64px;
   line-height: 84px;
 `
 
 const SmallHeader = styled.span`
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica,
-    Arial, sans-serif;
+  font-family: -apple-system, BNavbarLinkMacSystemFont, "Segoe UI", Roboto,
+    Helvetica, Arial, sans-serif;
   font-size: 31px;
   line-height: 36px;
   font-weight: 700;
@@ -100,6 +95,41 @@ const AuthButtonsWrapper = styled.div`
   display: flex;
   flex-direction: column;
   margin-top: 30px;
+`
+
+const NavbarBlock = styled.div`
+  height: 5vh;
+  display: flex;
+  justify-content: center;
+`
+
+const NavbarContainer = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  min-width: 1400px;
+`
+
+const NavbarLink = styled.a`
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica,
+    Arial, sans-serif;
+  font-size: 13px;
+  line-height: 16px;
+  font-weight: 400;
+  color: #5b7083;
+  :hover {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`
+
+const NavbarCopyrightSpan = styled.span`
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica,
+    Arial, sans-serif;
+  font-size: 13px;
+  line-height: 16px;
+  font-weight: 400;
+  color: #5b7083;
 `
 
 const Home: NextPage<{}> = () => {
@@ -136,59 +166,27 @@ const Home: NextPage<{}> = () => {
             </TwitterLogoWrapper>
           </ImagesBlock>
         </MainBlock>
-        {/* <NavbarBlock>
-          <ul>
-            <li>
-              <Link href="/">About</Link>
-            </li>
-            <li>
-              <Link href="/">Help Center</Link>
-            </li>
-            <li>
-              <Link href="/">Terms of Service</Link>
-            </li>
-            <li>
-              <Link href="/">Privacy Policy</Link>
-            </li>
-            <li>
-              <Link href="/">Cookie Policy</Link>
-            </li>
-            <li>
-              <Link href="/">Ads info</Link>
-            </li>
-            <li>
-              <Link href="/">Blog</Link>
-            </li>
-            <li>
-              <Link href="/">Status</Link>
-            </li>
-            <li>
-              <Link href="/">Careers</Link>
-            </li>
-            <li>
-              <Link href="/">Brand Resources</Link>
-            </li>
-            <li>
-              <Link href="/">Advertising</Link>
-            </li>
-            <li>
-              <Link href="/">Marketing</Link>
-            </li>
-            <li>
-              <Link href="/">Twitter for Business</Link>
-            </li>
-            <li>
-              <Link href="/">Developers</Link>
-            </li>
-            <li>
-              <Link href="/">Directory</Link>
-            </li>
-            <li>
-              <Link href="/">Settings</Link>
-            </li>
-          </ul>
-          <span>© 2021 Twitter, Inc.</span>
-        </NavbarBlock> */}
+        <NavbarBlock>
+          <NavbarContainer>
+            <NavbarLink href="/">About</NavbarLink>
+            <NavbarLink href="/">Help Center</NavbarLink>
+            <NavbarLink href="/">Terms of Service</NavbarLink>
+            <NavbarLink href="/">Privacy Policy</NavbarLink>
+            <NavbarLink href="/">Cookie Policy</NavbarLink>
+            <NavbarLink href="/">Ads info</NavbarLink>
+            <NavbarLink href="/">Blog</NavbarLink>
+            <NavbarLink href="/">Status</NavbarLink>
+            <NavbarLink href="/">Careers</NavbarLink>
+            <NavbarLink href="/">Brand Resources</NavbarLink>
+            <NavbarLink href="/">Advertising</NavbarLink>
+            <NavbarLink href="/">Marketing</NavbarLink>
+            <NavbarLink href="/">Twitter for Business</NavbarLink>
+            <NavbarLink href="/">Developers</NavbarLink>
+            <NavbarLink href="/">Directory</NavbarLink>
+            <NavbarLink href="/">Settings</NavbarLink>
+            <NavbarCopyrightSpan>© 2021 Twitter, Inc.</NavbarCopyrightSpan>
+          </NavbarContainer>
+        </NavbarBlock>
       </MainWrapper>
     </>
   )
